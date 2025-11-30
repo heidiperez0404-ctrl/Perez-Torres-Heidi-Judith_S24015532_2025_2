14060afaa1149930bf3a65d0393c968b59e77dce
heidiperez0404-ctrl <heidiperez0404@gmail.com>
2025-11-29 21:14:52 -0600

    Update and rename Ejercicio15.java to E15TemPais.java

--- E15TemPais.java
+++ E15TemPais.java
@@ -1,44 +1,48 @@
+//Ejercicio 15
+
 import java.util.Scanner;
 
-public class Ejercicio15 {
-    public static void main(String[] args) {
+public class E15TemPais 
+{
+    public static void main(String[] args) 
+    {
     
-        Scanner input = new Scanner(System.in);
+        Scanner tp = new Scanner(System.in);
         String paises[] = new String[4];
         double temp[][] = new double[4][4];
         double tempTrimestral = 0;
         
-        for(int i = 0; i < paises.length; i++) {
+        for(int i = 0; i < paises.length; i++) 
+        {
             System.out.print("Ingresa el nombre del pais " + (i + 1) + ": ");
-            paises[i] = input.nextLine();
+            paises[i] = tp.nextLine();
             
-            for(int j = 0; j < 3; j++) {
+            for(int j = 0; j < 3; j++) 
+            {
                 System.out.print("Ingresa la temperatura media del  " + (j + 1) + " mes del pais " + paises[i] + ": ");
-                temp[i][j] = Double.parseDouble(input.nextLine());
+                temp[i][j] = Double.parseDouble(tp.nextLine());
                 tempTrimestral += temp[i][j];
-            
             }
             temp[i][3] = tempTrimestral / 3;
             tempTrimestral = 0;
         }
         
         System.out.println("\tInformacion de los paises");
-        System.out.printf("%-15s %-8s %-8s %-8s %-8s\n", 
-            "Pais", "Temp1", "Temp2", "Temp3", "Media");
+        System.out.printf("%-15s %-8s %-8s %-8s %-8s\n", "Pais", "Temp1", "Temp2", "Temp3", "Media");
             
-            for(int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++) 
+            {
                 System.out.printf("%-15s ", paises[i]);
     
-                for(int j = 0; j < 4; j++) {
+                for(int j = 0; j < 4; j++) 
+                {
                     System.out.printf("%-8.2f ", temp[i][j]);
                 
                 }
                 System.out.println("");
             }
-            
-            
-            input.close();
         
+            tp.close();  
     
     }
 }
