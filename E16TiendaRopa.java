2451426521517738630e04fa4f7b762bc1f3544f
heidiperez0404-ctrl <heidiperez0404@gmail.com>
2025-11-29 21:29:35 -0600

    Replace Scanner variable and clean up code

--- E16TiendaRopa.java
+++ E16TiendaRopa.java
@@ -1,23 +1,30 @@
+//Ejercicio 16
+
 import java.util.Scanner;
 
-public class Ejercicio16 {
-    public static void main(String[] args) {
+public class E16TiendaRopa 
+{
+    public static void main(String[] args) 
+    {
     
-        Scanner input = new Scanner(System.in);
+        Scanner tp = new Scanner(System.in);
         String articulos[][] = new String[3][4];  
         
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < 3; i++) 
+        {
             System.out.print("Ingresa el nombre del articulo " + (i + 1) + ": ");
-            articulos[i][0] = input.nextLine();
+            articulos[i][0] = tp.nextLine();
         
-            for(int j = 1; j < 4; j++) {
-                switch(j) {
+            for(int j = 1; j < 4; j++) 
+            {
+                switch(j) 
+                {
                     case 1 -> System.out.print("Ingresa la talla del articulo: ");
                     case 2 -> System.out.print("Ingresa el color del articulo: ");
                     case 3 -> System.out.print("Ingresa el precio del articulo: ");
                 }
                 
-                articulos[i][j] = input.nextLine();
+                articulos[i][j] = tp.nextLine();
             }
         }
         
@@ -25,14 +32,16 @@ public class Ejercicio16 {
         System.out.printf("%-10s %-8s %-12s %-10s\n", 
             "Artículo", "Talla", "Color", "Precio");
         
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 4; j++) {
+        for(int i = 0; i < 3; i++) 
+        {
+            for(int j = 0; j < 4; j++) 
+            {
                 System.out.printf("%-10s ", articulos[i][j]);
             }
             System.out.println();
         }
         
-        input.close();
+        tp.close();
     }
 }
 
