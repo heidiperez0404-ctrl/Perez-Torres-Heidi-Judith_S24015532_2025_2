9fcc6314678a7fb60f0e536f22fd166741cc13ec
heidiperez0404-ctrl <heidiperez0404@gmail.com>
2025-11-29 21:49:09 -0600

    Add files via upload

---
+++ E2NumWhi.java
@@ -0,0 +1,15 @@
+//Desarrolla un programa que imprima todos los números pares entre 1 y 20, utilizando un ciclo while.
+
+class E2NumWhi
+{
+	public static void main(String args[])
+	{
+	int num = 1;
+
+	while(num <= 20)
+	{
+		System.out.println(num);
+		num++;
+		}
+	}
+}
