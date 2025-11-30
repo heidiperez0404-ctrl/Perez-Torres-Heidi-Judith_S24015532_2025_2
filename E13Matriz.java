0050b05ba1cdcb15354d775513811b737f12b9d0
heidiperez0404-ctrl <heidiperez0404@gmail.com>
2025-11-29 21:09:48 -0600

    Add new file E13Matriz.java

--- E13Matriz.java
+++ E13Matriz.java
@@ -1,45 +1,54 @@
 import java.util.Scanner;
 
-public class Ejercicio13 {
-    public static void main(String[] args) {
+public class E13Matriz 
+{
+    public static void main(String[] args) 
+    {
     
-        Scanner input = new Scanner(System.in);   
+        Scanner mt = new Scanner(System.in);   
         String alumnos[][] = new String[3][4];
         
-        for(int nivel = 0; nivel < 3; nivel++) {
-            for(int idioma = 0; idioma < 4; idioma++) {
+        for(int nivel = 0; nivel < 3; nivel++) 
+        {
+            for(int idioma = 0; idioma < 4; idioma++) 
+            {
                 System.out.println("\tIngresa al alumno");
-                switch(nivel) {
+                switch(nivel) 
+                {
                     case 0 -> System.out.print("Alumno de nivel bajo ");
                     case 1 -> System.out.print("Alumno de nivel medio ");
                     case 2 -> System.out.print("Alumno de nivel de perfeccionamiento ");
                         
                 }
                 
-                switch(idioma) {
+                switch(idioma) 
+                {
                     case 0 -> System.out.print("que estudia Ingles: ");
                     case 1 -> System.out.print("que estudia Frances: ");
                     case 2 -> System.out.print("que estudia Aleman: ");
                     case 3 -> System.out.print("que estudia Ruso: ");
-                
                 }
                 
-                alumnos[nivel][idioma] = input.nextLine();
+                alumnos[nivel][idioma] = mt.nextLine();
                 
             }
         }
         
         System.out.println("\tMostrando datos de los alumnos...");
-        for(int nivel = 0; nivel < 3; nivel++) {
-            for(int idioma = 0; idioma < 4; idioma++) {
-                switch(nivel) {
+        for(int nivel = 0; nivel < 3; nivel++) 
+        {
+            for(int idioma = 0; idioma < 4; idioma++) 
+            {
+                switch(nivel) 
+                {
                     case 0 -> System.out.print("Alumno con nivel bajo ");
                     case 1 -> System.out.print("Alumno con nivel medio ");
                     case 2 -> System.out.print("Alumno con nivel de perfeccionamiento ");
                         
                 }
                 
-                switch(idioma) {
+                switch(idioma) 
+                {
                     case 0 -> System.out.print("de Ingles: ");
                     case 1 -> System.out.print("de Frances: ");
                     case 2 -> System.out.print("de Aleman: ");
@@ -52,7 +61,7 @@ public class Ejercicio13 {
             }
         }
         
-        input.close();
+        mt.close();
             
     
     }
